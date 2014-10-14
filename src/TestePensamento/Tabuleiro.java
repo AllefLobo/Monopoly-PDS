b47f9b3b7d51ebd5fb50bdfee1a613f453e551b6
AllefLobo <allef.lobo@gmail.com>
2014-10-14 11:29:20 -0300

    novo pensamento

---
+++ src/TestePensamento/Tabuleiro.java
@@ -0,0 +1,53 @@
+package TestePensamento;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Tabuleiro {
+	
+	private static final int TAMANHO = 40;
+	private List casas = new ArrayList(TAMANHO);
+	
+	public Tabuleiro() {
+		criarCasas();
+		ligarCasas();
+	}
+	
+	public Casa obterCasa(Casa iniciar, int distancia){
+		int indiceFinal = (iniciar.obterIndice() + distancia) % TAMANHO;
+		return (Casa) casas.get(indiceFinal);
+	}
+	
+	public Casa obterCasaInicial(){
+		return (Casa) casas.get(0);
+	}
+	
+	private void criarCasas(){
+		for(int i=1; i <= TAMANHO; i++)
+			criar(i);
+	}
+	
+	private void criar(int i){
+		Casa c = new Casa("Casa" + i, i-1);
+	}
+	
+	private void ligarCasas(){
+		for(int i = 0; i< (TAMANHO-1) ;i++)
+			ligar(i);
+		
+		Casa primeira = obterCasaInicial();
+		Casa ultima = (Casa) casas.get(TAMANHO-1);
+		ultima.estabelecerProximaCasa(primeira);
+		
+	}
+	
+	private void ligar(int i){
+		Casa corrente  = (Casa) casas.get(i);
+		Casa proxima = (Casa) casas.get(i+1);
+		corrente.estabelecerProximaCasa(proxima);
+	}
+	
+	
+	
+	
+}
